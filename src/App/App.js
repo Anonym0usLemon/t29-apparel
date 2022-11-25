cc424bef86fbf1c332f7af9bd5adb8bed22dfaab
Anonym0usLemon <anonym0uslemon@Anonym0emonsMBP.lan>
2022-11-25 15:50:41 -0600

    added box-shadow and background styles for main content

--- src/App/App.js
+++ src/App/App.js
@@ -14,57 +14,61 @@ import Influencers from "../components/Influencers";
 class App extends Component {
     render() {
         return (
-		<div className="page-body">
-			<HashRouter>
-					<header id="nav">
-					<h1 id="title">T29 Apparel</h1>
-				{ /* <!-- Line separating   title from Nav--> */ }
-					<div className="navContainer">
-						<div className="navBar"></div> 
-						<div className="space1"></div>
-						<div className="navBar3"></div>
-						<div className="space2"></div>
-						<div className="navBar5"></div>
-						<div className="triangleContainer">
-							<div class="triangle"></div>
+		<div className="page-container">
+			<div className="before"></div>
+			<div className="page-body">
+				<HashRouter>
+						<header id="nav">
+						<h1 id="title">T29 Apparel</h1>
+					{ /* <!-- Line separating   title from Nav--> */ }
+						<div className="navContainer">
+							<div className="navBar"></div> 
+							<div className="space1"></div>
+							<div className="navBar3"></div>
+							<div className="space2"></div>
+							<div className="navBar5"></div>
+							<div className="triangleContainer">
+								<div class="triangle"></div>
+							</div>
 						</div>
+					{ /* <!--Nav Items-->  */ }
+						<div id="nav-links">
+							<NavLink to="/">Home</NavLink>
+							<div className="itemSpace"></div>
+							<NavLink to="/about">About</NavLink>
+							<div className="itemSpace"></div>
+							<NavLink to="/Store">Store</NavLink>
+							<div className="itemSpace"></div>
+							<NavLink to="/Events">Events</NavLink>
+							<div className="itemSpace"></div>
+							<NavLink to="/Influencers">Influencers</NavLink>
+						</div>
+					</header>
+				{ /* <!-- Page Content --> */ }
+					<div className="content">
+						<Routes>
+							<Route exact path="/" element={<Home />}/>
+							<Route exact path="/about" element={<About />}/>
+							<Route exact path="/store" element={<Store />}/>
+							<Route exact path="/events" element={<Events />}/>
+							<Route exact path="/influencers" element={<Influencers />}/>
+						</Routes>
 					</div>
-				{ /* <!--Nav Items-->  */ }
-					<div id="nav-links">
-						<NavLink to="/">Home</NavLink>
-						<div className="itemSpace"></div>
-						<NavLink to="/about">About</NavLink>
-						<div className="itemSpace"></div>
-						<NavLink to="/Store">Store</NavLink>
-						<div className="itemSpace"></div>
-						<NavLink to="/Events">Events</NavLink>
-						<div className="itemSpace"></div>
-						<NavLink to="/Influencers">Influencers</NavLink>
-					</div>
-				</header>
-			{ /* <!-- Page Content --> */ }
-				<div className="content">
-					<Routes>
-						<Route exact path="/" element={<Home />}/>
-						<Route exact path="/about" element={<About />}/>
-						<Route exact path="/store" element={<Store />}/>
-						<Route exact path="/events" element={<Events />}/>
-						<Route exact path="/influencers" element={<Influencers />}/>
-					</Routes>
-				</div>
-				<footer>
-					<div className="footer-links">
-						<a href="#">T29 Apparel</a>
-						<br/>
-						<a href="#">Support</a>
-					</div>
-					<div className="newsletter">
-						<h6>Sign up for our newsletter</h6>
-						<input type="text" value="Enter your Email"></input>
-						<button>Sign up</button>
-					</div>
-				</footer>
-			</HashRouter>
+					<footer>
+						<div className="footer-links">
+							<a href="#">T29 Apparel</a>
+							<br/>
+							<a href="#">Support</a>
+						</div>
+						<div className="newsletter">
+							<h6>Sign up for our newsletter</h6>
+							<input type="text" value="Enter your Email"></input>
+							<button>Sign up</button>
+						</div>
+					</footer>
+				</HashRouter>
+			</div>
+			<div className="after"></div>
 		</div>
 		)
     }
